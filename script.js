a02bab314bebfa23ff2fd92cc84c3720d93111fa
Poojitha Levaku <poojithareddy2k07@gmail.com>
2024-12-23 18:04:28 +0530

    Day 3: UI Enhancements, Project Cards, ContactForms

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 document.getElementById("alertButton").addEventListener("click", function() {
-    alert("Hello");
+  alert("Hello");
+});
+
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault(); 
+    const targetId = link.getAttribute('href'); 
+    document.querySelector(targetId).scrollIntoView({
+      behavior: 'smooth' 
+    });
   });
-
-
-
-
+});
+const contactForm = document.getElementById('contactForm');
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  alert('Thanks for reaching out!');
+  contactForm.reset();
+});
